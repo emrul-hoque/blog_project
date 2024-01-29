d7af5d42d39735a68e554cafe8242b2cfb160fd5
Emrul <emrul.hoque@gmail.com>
2024-01-29 19:56:01 +0600

    initial commit

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -11,4 +13,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+    # context_object_name = 'post'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+    # context_object_name = 'post'
+
+class BlogUpdateView(UpdateView):   
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    # context_object_name = 'post'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
     # context_object_name = 'post'
