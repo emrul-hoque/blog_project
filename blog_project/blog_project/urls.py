4852d84c4226cb611f6b4adc9dd6ada88d7d6149
Emrul <emrul.hoque@gmail.com>
2024-02-02 11:15:40 +0600

    "update"

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
