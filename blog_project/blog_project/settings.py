602b2188c51f4c5e12fcec0abbe8496120e62bda
Emrul <emrul.hoque@gmail.com>
2024-02-04 19:51:17 +0600

    "user_accounts"

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "blog.apps.BlogConfig", # eBook
+    "blog.apps.BlogConfig", 
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
